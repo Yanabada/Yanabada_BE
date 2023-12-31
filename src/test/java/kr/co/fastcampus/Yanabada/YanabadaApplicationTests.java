cb35ed2b5aa50a0c5db85a6b52b622b63929a38e
JuHyeonPark <parkjh080@naver.com>
2023-12-31 20:18:43 +0900

    feat: 초기셋팅
    초기셋팅

---
+++ src/test/java/kr/co/fastcampus/Yanabada/YanabadaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.Yanabada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanabadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
