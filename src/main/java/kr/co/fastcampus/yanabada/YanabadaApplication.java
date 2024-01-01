f27bf6b445f4b92cdd3e6c1125283adf3a10fd62
JuHyeonPark <parkjh080@naver.com>
2024-01-01 20:30:29 +0900

    refactor: @EnableJpaAuditing  어노테이션 추가
    생성날짜, 수정날짜 자동 생성을 위한 @EnableJpaAuditing 추가

--- src/main/java/kr/co/fastcampus/yanabada/YanabadaApplication.java
+++ src/main/java/kr/co/fastcampus/yanabada/YanabadaApplication.java
@@ -2,8 +2,10 @@ package kr.co.fastcampus.yanabada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YanabadaApplication {
 
 	public static void main(String[] args) {
