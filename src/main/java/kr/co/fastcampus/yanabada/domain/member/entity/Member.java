db041a945e95d56c4fb726bc5439ac8868d798e9
KyuCheol Hwang <iambigman97@naver.com>
2024-01-02 21:39:33 +0900

    feat: Member Entity에 ID 추가
    Member Entity에 ID 추가

--- src/main/java/kr/co/fastcampus/yanabada/domain/member/entity/Member.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/member/entity/Member.java
@@ -1,5 +1,14 @@
 package kr.co.fastcampus.yanabada.domain.member.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
