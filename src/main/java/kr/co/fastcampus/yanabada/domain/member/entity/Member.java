50791040dc062d30594da12f98b708815b2260e4
KyuCheol Hwang <iambigman97@naver.com>
2024-01-02 21:25:40 +0900

    feat: 객실 예약 entity 구현
    RoomOrder Entity 구현

---
+++ src/main/java/kr/co/fastcampus/yanabada/domain/member/entity/Member.java
@@ -0,0 +1,5 @@
+package kr.co.fastcampus.yanabada.domain.member.entity;
+
+public class Member {
+
+}
---
+++ src/main/java/kr/co/fastcampus/yanabada/domain/order/entity/enums/PaymentType.java
@@ -0,0 +1,5 @@
+package kr.co.fastcampus.yanabada.domain.order.entity.enums;
+
+public enum PaymentType {
+    NAVER_PAY
+}
