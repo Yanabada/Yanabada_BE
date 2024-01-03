eb1006f970cefe010f4244155bacd60e6d8abf18
KyuCheol Hwang <iambigman97@naver.com>
2024-01-03 11:14:56 +0900

    feat: 숙소 옵션, 룸 옵션 Entity 구현
    AccommodationOption, RoomOption Entity 구현

---
+++ src/main/java/kr/co/fastcampus/yanabada/domain/accommodation/entity/RoomOption.java
@@ -0,0 +1,26 @@
+package kr.co.fastcampus.yanabada.domain.accommodation.entity;
+
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "room_option")
+public class RoomOption {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+}
