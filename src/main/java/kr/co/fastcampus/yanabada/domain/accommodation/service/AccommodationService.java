2d8263c141e58cdad950d0dc39203eb35c02cd94
JuHyeonPark <parkjh080@naver.com>
2024-01-04 13:36:18 +0900

    feat: 숙박 서비스 계층 구현
    숙박 서비스 계층 구현 -  숙박, 객실 등록 api 작성

---
+++ src/main/java/kr/co/fastcampus/yanabada/domain/accommodation/service/AccommodationService.java
@@ -0,0 +1,36 @@
+package kr.co.fastcampus.yanabada.domain.accommodation.service;
+
+import java.util.List;
+import kr.co.fastcampus.yanabada.domain.accommodation.dto.request.AccommodationSaveRequest;
+import kr.co.fastcampus.yanabada.domain.accommodation.dto.request.RoomSaveRequest;
+import kr.co.fastcampus.yanabada.domain.accommodation.entity.Accommodation;
+import kr.co.fastcampus.yanabada.domain.accommodation.repository.AccommodationRepository;
+import kr.co.fastcampus.yanabada.domain.accommodation.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AccommodationService {
+
+    private final AccommodationRepository accommodationRepository;
+
+    private final RoomRepository roomRepository;
+
+    @Transactional
+    public void saveAccommodations(List<AccommodationSaveRequest> requests) {
+        for (AccommodationSaveRequest request : requests) {
+            Accommodation accommodation = accommodationRepository.save(request.toEntity());
+        }
+    }
+
+    @Transactional
+    public void saveRooms(List<RoomSaveRequest> requests) {
+        for (RoomSaveRequest request : requests) {
+            Accommodation accommodation =
+                accommodationRepository.getAccommodation(request.accommodationId());
+            accommodation.addRoom(request.toEntity(accommodation));
+        }
+    }
+}
