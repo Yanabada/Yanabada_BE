2697d6db7bbd1a047233b9d0c05a734c169c3c8e
JuHyeonPark <parkjh080@naver.com>
2024-01-04 13:41:35 +0900

    feat: 주문 등록 dto 구현
    주문 등록 dto 구현

---
+++ src/main/java/kr/co/fastcampus/yanabada/domain/order/dto/request/OrderSaveRequest.java
@@ -0,0 +1,44 @@
+package kr.co.fastcampus.yanabada.domain.order.dto.request;
+
+import java.time.LocalDate;
+
+import kr.co.fastcampus.yanabada.domain.accommodation.entity.Room;
+import kr.co.fastcampus.yanabada.domain.member.entity.Member;
+import kr.co.fastcampus.yanabada.domain.order.entity.RoomOrder;
+import kr.co.fastcampus.yanabada.domain.order.entity.enums.OrderStatus;
+import kr.co.fastcampus.yanabada.domain.order.entity.enums.PaymentType;
+
+public record OrderSaveRequest(
+    Long roomId,
+    Long memberId,
+    LocalDate checkInDate,
+    LocalDate checkOutDate,
+    OrderStatus status,
+    Integer price,
+    String reservationPersonName,
+    String reservationPersonPhoneNumber,
+    String userPersonName,
+    String userPersonPhoneNumber,
+    PaymentType paymentType,
+    Integer adultCount,
+    Integer childCount
+) {
+
+    public RoomOrder toEntity(Room room, Member member) {
+        return RoomOrder.create(
+            room,
+            member,
+            checkInDate,
+            checkOutDate,
+            status,
+            price,
+            reservationPersonName,
+            reservationPersonPhoneNumber,
+            userPersonName,
+            userPersonPhoneNumber,
+            paymentType,
+            adultCount,
+            childCount
+        );
+    }
+}
