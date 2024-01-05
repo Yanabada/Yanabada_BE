8f822a0992d96002fb4a55f05f25f75b3ba62254
tjdtn0219 <s_sjang@naver.com>
2024-01-06 01:01:09 +0900

    feat: 멤버 Entity 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/member/entity/Member.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/member/entity/Member.java
@@ -1,15 +1,29 @@
 package kr.co.fastcampus.yanabada.domain.member.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import kr.co.fastcampus.yanabada.common.baseentity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 @Entity
 public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String email;
+    private String memberName;
+    private String nickName;
+    private String password;
+    private String phoneNumber;
+    private String imageUrl;
+    private Integer point;
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    @Enumerated(EnumType.STRING)
+    private ProviderType providerType;
+    private String deviceKey;
+
 }
---
+++ src/main/java/kr/co/fastcampus/yanabada/domain/member/entity/ProviderType.java
@@ -0,0 +1,5 @@
+package kr.co.fastcampus.yanabada.domain.member.entity;
+
+public enum ProviderType {
+    EMAIL, KAKAO
+}
