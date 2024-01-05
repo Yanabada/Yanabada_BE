d2f4388529d6032179a18fb01ad9d2619bd338e6
KyuCheol Hwang <iambigman97@naver.com>
2024-01-05 21:53:22 +0900

    feat: Product 등록의 요청 및 응답 DTO 구현
    ProductSaveRequest, ProductIdResponse 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/product/dto/request/ProductSaveRequest.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/product/dto/request/ProductSaveRequest.java
@@ -3,6 +3,9 @@ package kr.co.fastcampus.yanabada.domain.product.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
+import kr.co.fastcampus.yanabada.domain.order.entity.Order;
+import kr.co.fastcampus.yanabada.domain.product.entity.Product;
+import kr.co.fastcampus.yanabada.domain.product.entity.enums.ProductStatus;
 
 public record ProductSaveRequest(
 
@@ -25,4 +28,17 @@ public record ProductSaveRequest(
     Boolean isAutoCancel
 ) {
 
+    public Product toEntity(
+        Order order
+    ) {
+        return Product.create(
+            order,
+            price,
+            description,
+            canNegotiate,
+            saleEndDate,
+            isAutoCancel,
+            ProductStatus.ON_SALE
+        );
+    }
 }
--- src/main/java/kr/co/fastcampus/yanabada/domain/product/dto/response/ProductIdResponse.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/product/dto/response/ProductIdResponse.java
@@ -1,7 +1,12 @@
 package kr.co.fastcampus.yanabada.domain.product.dto.response;
 
+import kr.co.fastcampus.yanabada.domain.product.entity.Product;
+
 public record ProductIdResponse(
     Long productId
 ) {
 
+    public ProductIdResponse(Product product) {
+        this(product.getId());
+    }
 }
