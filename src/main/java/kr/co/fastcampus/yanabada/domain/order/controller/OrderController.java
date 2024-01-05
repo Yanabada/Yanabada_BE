8bf1cf8c6e5391fb190c28a943622790c048fe02
deltawing71911 <deltawing71911@gmail.com>
2024-01-06 00:05:16 +0900

    feat: 예약내역 상세 컨트롤러 구현 resolved #14

--- src/main/java/kr/co/fastcampus/yanabada/domain/order/controller/OrderController.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/order/controller/OrderController.java
@@ -2,9 +2,12 @@ package kr.co.fastcampus.yanabada.domain.order.controller;
 
 import kr.co.fastcampus.yanabada.common.response.ResponseBody;
 import kr.co.fastcampus.yanabada.domain.order.dto.request.OrderSaveRequest;
+import kr.co.fastcampus.yanabada.domain.order.dto.response.ReservationDetailsDto;
 import kr.co.fastcampus.yanabada.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,12 @@ public class OrderController {
     public ResponseBody<Void> addOrder(@RequestBody OrderSaveRequest request) {
         orderService.saveOrder(request);
         return ResponseBody.ok();
+    }
+
+    @GetMapping("/{reservationId}")
+    public ResponseBody<ReservationDetailsDto> getReservationDetails(@PathVariable Long reservationId) {
+        ReservationDetailsDto reservationDetailsDto = orderService.getReservationDetails(reservationId);
+        return ResponseBody.ok(reservationDetailsDto);
     }
 
 }
