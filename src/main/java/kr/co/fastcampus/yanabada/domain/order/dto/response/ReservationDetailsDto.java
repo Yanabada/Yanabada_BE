884289194dd0c4b1c5ff6b9be1caad0e2297af1e
deltawing71911 <deltawing71911@gmail.com>
2024-01-06 00:02:05 +0900

    feat: 예약내역 상세 응답 DTO 구현, 서비스 계층 구현 resolved #14

---
+++ src/main/java/kr/co/fastcampus/yanabada/domain/order/dto/response/ReservationDetailsDto.java
@@ -0,0 +1,32 @@
+package kr.co.fastcampus.yanabada.domain.order.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import java.time.LocalDate;
+import java.util.Map;
+
+@Getter
+@Builder
+public class ReservationDetailsDto {
+
+    private final Long reservationId;
+    private final LocalDate reservationDate;
+    private final String accommodationName;
+    private final String accommodationImage;
+    private final String roomName;
+    private final LocalDate checkInDate;
+    private final LocalDate checkOutDate;
+    private final Integer price;
+    private final Integer adultCount;
+    private final Integer childCount;
+    private final Integer maxHeadCount;
+    private final String reservationPersonName;
+    private final String reservationPersonPhoneNumber;
+    private final String userPersonName;
+    private final String userPersonPhoneNumber;
+    private final Map<String, Boolean> roomOptions;
+    private final Integer totalPayment;
+    private final String paymentMethod;
+
+}
+
--- src/main/java/kr/co/fastcampus/yanabada/domain/order/service/OrderService.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/order/service/OrderService.java
@@ -1,10 +1,12 @@
 package kr.co.fastcampus.yanabada.domain.order.service;
 
+import kr.co.fastcampus.yanabada.common.exception.RoomNotFoundException;
 import kr.co.fastcampus.yanabada.domain.accommodation.entity.Room;
 import kr.co.fastcampus.yanabada.domain.accommodation.repository.RoomRepository;
 import kr.co.fastcampus.yanabada.domain.member.entity.Member;
 import kr.co.fastcampus.yanabada.domain.member.repository.MemberRepository;
 import kr.co.fastcampus.yanabada.domain.order.dto.request.OrderSaveRequest;
+import kr.co.fastcampus.yanabada.domain.order.dto.response.ReservationDetailsDto;
 import kr.co.fastcampus.yanabada.domain.order.entity.RoomOrder;
 import kr.co.fastcampus.yanabada.domain.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
@@ -26,5 +28,32 @@ public class OrderService {
         Room room = roomRepository.getRoom(request.roomId());
         Member member = memberRepository.getMember(request.memberId());
         RoomOrder roomOrder = orderRepository.save(request.toEntity(room, member));
+    }
+
+    @Transactional
+    public ReservationDetailsDto getReservationDetails(Long reservationId) {
+        RoomOrder roomOrder = orderRepository.findById(reservationId)
+            .orElseThrow(RoomNotFoundException::new);
+
+        return ReservationDetailsDto.builder()
+            .reservationId(roomOrder.getId())
+            .reservationDate(roomOrder.getCheckInDate())
+            .accommodationName(roomOrder.getRoom().getAccommodation().getName())
+            .accommodationImage(roomOrder.getRoom().getAccommodation().getImage())
+            .roomName(roomOrder.getRoom().getName())
+            .checkInDate(roomOrder.getCheckInDate())
+            .checkOutDate(roomOrder.getCheckOutDate())
+            .price(roomOrder.getPrice())
+            .adultCount(roomOrder.getAdultCount())
+            .childCount(roomOrder.getChildCount())
+            .maxHeadCount(roomOrder.getRoom().getMaxHeadCount())
+            .reservationPersonName(roomOrder.getReservationPersonName())
+            .reservationPersonPhoneNumber(roomOrder.getReservationPersonPhoneNumber())
+            .userPersonName(roomOrder.getUserPersonName())
+            .userPersonPhoneNumber(roomOrder.getUserPersonPhoneNumber())
+            // RoomOptions는 현재 entity가 구현되지 않은 상태라 맵을 추가하지 않았습니다.
+            .totalPayment(roomOrder.getPrice())
+            .paymentMethod(roomOrder.getPaymentType().name())
+            .build();
     }
 }
