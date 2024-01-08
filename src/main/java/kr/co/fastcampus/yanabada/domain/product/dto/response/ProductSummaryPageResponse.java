6f13e429cf99182d49c9e046d8494f1a122d5e16
KyuCheol Hwang <iambigman97@naver.com>
2024-01-08 17:28:46 +0900

    feat: 상품 검색 response dto 의 생성 메서드 구현
    상품 검색 response dto 의 from 메소드 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/product/dto/response/ProductSummaryPageResponse.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/product/dto/response/ProductSummaryPageResponse.java
@@ -1,7 +1,28 @@
 package kr.co.fastcampus.yanabada.domain.product.dto.response;
 
-public record ProductSummaryPageResponse(
+import java.util.List;
+import java.util.stream.Collectors;
+import kr.co.fastcampus.yanabada.domain.product.entity.Product;
+import lombok.Builder;
+import org.springframework.data.domain.Page;
 
+@Builder
+public record ProductSummaryPageResponse(
+    List<ProductSummaryResponse> products,
+    long totalElements,
+    int totalPages
 ) {
 
+    public static ProductSummaryPageResponse from(Page<Product> page) {
+        return ProductSummaryPageResponse.builder()
+            .products(
+                page.getContent()
+                    .stream()
+                    .map(ProductSummaryResponse::from)
+                    .collect(Collectors.toList())
+            )
+            .totalElements(page.getTotalElements())
+            .totalPages(page.getTotalPages())
+            .build();
+    }
 }
