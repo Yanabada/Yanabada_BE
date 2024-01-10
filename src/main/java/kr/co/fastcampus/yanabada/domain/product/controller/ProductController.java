e06e6700ae6c7a85dad118ae7db7e9b55f3d0e97
KyuCheol Hwang <iambigman97@naver.com>
2024-01-10 12:26:58 +0900

    feat: ProductController 의 상품 조회 메소드 구현
    ProductController 의 getProduct 메소드 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/product/controller/ProductController.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/product/controller/ProductController.java
@@ -4,9 +4,12 @@ import jakarta.validation.Valid;
 import kr.co.fastcampus.yanabada.common.response.ResponseBody;
 import kr.co.fastcampus.yanabada.domain.product.dto.request.ProductSaveRequest;
 import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductIdResponse;
+import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductInfoResponse;
 import kr.co.fastcampus.yanabada.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +29,15 @@ public class ProductController {
     ) {
         return ResponseBody.ok(
             productService.saveProduct(1L, request)
+        );
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseBody<ProductInfoResponse> getProduct(
+        @PathVariable("productId") Long productId
+    ) {
+        return ResponseBody.ok(
+            productService.getProductById(productId)
         );
     }
 }
