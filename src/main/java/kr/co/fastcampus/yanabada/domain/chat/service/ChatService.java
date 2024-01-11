709aed7a1fdf8728483a7733072c518508e16f16
JuHyeonPark <parkjh080@naver.com>
2024-01-11 15:23:49 +0900

    feat: 채팅방 서비스 계층 구축
    채팅방 서비스 계층 구축

---
+++ src/main/java/kr/co/fastcampus/yanabada/domain/chat/service/ChatService.java
@@ -0,0 +1,29 @@
+package kr.co.fastcampus.yanabada.domain.chat.service;
+
+import kr.co.fastcampus.yanabada.domain.chat.dto.request.ChatRoomSaveRequest;
+import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomIdResponse;
+import kr.co.fastcampus.yanabada.domain.chat.repository.ChatMessageRepository;
+import kr.co.fastcampus.yanabada.domain.chat.repository.ChatRoomRepository;
+import kr.co.fastcampus.yanabada.domain.member.repository.MemberRepository;
+import kr.co.fastcampus.yanabada.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+    private final ChatRoomRepository chatRoomRepository;
+
+    private final ChatMessageRepository chatMessageRepository;
+
+    private final MemberRepository memberRepository;
+
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public ChatRoomIdResponse saveChatRoom(ChatRoomSaveRequest request) {
+        return null;
+    }
+}
