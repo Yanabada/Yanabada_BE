627ee22811f44b5a336a6921c0c9b43aaa71dd5f
JuHyeonPark <parkjh080@naver.com>
2024-01-11 16:18:48 +0900

    feat: 채팅방 등록 api 구현
    채팅방 등록 api 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/chat/service/ChatService.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/chat/service/ChatService.java
@@ -1,10 +1,14 @@
 package kr.co.fastcampus.yanabada.domain.chat.service;
 
+import java.time.LocalDateTime;
 import kr.co.fastcampus.yanabada.domain.chat.dto.request.ChatRoomSaveRequest;
 import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomIdResponse;
+import kr.co.fastcampus.yanabada.domain.chat.entity.ChatRoom;
 import kr.co.fastcampus.yanabada.domain.chat.repository.ChatMessageRepository;
 import kr.co.fastcampus.yanabada.domain.chat.repository.ChatRoomRepository;
+import kr.co.fastcampus.yanabada.domain.member.entity.Member;
 import kr.co.fastcampus.yanabada.domain.member.repository.MemberRepository;
+import kr.co.fastcampus.yanabada.domain.product.entity.Product;
 import kr.co.fastcampus.yanabada.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,6 +28,13 @@ public class ChatService {
 
     @Transactional
     public ChatRoomIdResponse saveChatRoom(ChatRoomSaveRequest request) {
-        return null;
+        Product product = productRepository.getProduct(request.productId());
+        Member seller = memberRepository.getMember(product.getOrder().getMember().getId());
+        Member buyer = memberRepository.getMember(request.buyerId());
+        LocalDateTime now = LocalDateTime.now();
+        ChatRoom chatRoom =chatRoomRepository.save(
+            request.toEntity(product, seller, buyer, now, now)
+        );
+        return ChatRoomIdResponse.from(chatRoom);
     }
 }
