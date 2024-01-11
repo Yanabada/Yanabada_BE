8fb5bf0ee4dec4ca89e3995f12abf2cd73b5f253
KyuCheol Hwang <iambigman97@naver.com>
2024-01-11 23:28:32 +0900

    feat: ProductService 의 상품 수정 메소드 구현
    patchProduct 메소드 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/product/service/ProductService.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/product/service/ProductService.java
@@ -1,8 +1,13 @@
 package kr.co.fastcampus.yanabada.domain.product.service;
 
+import static kr.co.fastcampus.yanabada.domain.product.entity.enums.ProductStatus.CANCELED;
+import static kr.co.fastcampus.yanabada.domain.product.entity.enums.ProductStatus.SOLD_OUT;
+
+import io.micrometer.common.util.StringUtils;
 import java.time.LocalDate;
 import java.util.Objects;
 import kr.co.fastcampus.yanabada.common.exception.AccessForbiddenException;
+import kr.co.fastcampus.yanabada.common.exception.InvalidStatusProductUpdateException;
 import kr.co.fastcampus.yanabada.common.exception.OrderNotFoundException;
 import kr.co.fastcampus.yanabada.common.exception.OrderNotSellableException;
 import kr.co.fastcampus.yanabada.common.exception.SaleEndDateRangeException;
@@ -12,11 +17,13 @@ import kr.co.fastcampus.yanabada.domain.member.repository.MemberRepository;
 import kr.co.fastcampus.yanabada.domain.order.entity.Order;
 import kr.co.fastcampus.yanabada.domain.order.entity.enums.OrderStatus;
 import kr.co.fastcampus.yanabada.domain.order.repository.OrderRepository;
+import kr.co.fastcampus.yanabada.domain.product.dto.request.ProductPatchRequest;
 import kr.co.fastcampus.yanabada.domain.product.dto.request.ProductSaveRequest;
 import kr.co.fastcampus.yanabada.domain.product.dto.request.ProductSearchRequest;
 import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductIdResponse;
 import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductInfoResponse;
 import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductSummaryPageResponse;
+import kr.co.fastcampus.yanabada.domain.product.entity.Product;
 import kr.co.fastcampus.yanabada.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -65,6 +72,48 @@ public class ProductService {
         );
     }
 
+    @Transactional
+    public ProductIdResponse patchProduct(
+        Long memberId,
+        Long productId,
+        ProductPatchRequest request
+    ) {
+        Member member = memberRepository.getMember(memberId);
+        Product product = productRepository.getProduct(productId);
+        Order order = product.getOrder();
+
+        if (!Objects.equals(member, order.getMember())) {
+            throw new AccessForbiddenException();
+        }
+
+        validateSellableOrder(order);
+        validateUpdatableProduct(product);
+
+        if (request.price() != null) {
+            validateProductPrice(request.price(), order.getPrice());
+            product.updatePrice(request.price());
+        }
+
+        if (StringUtils.isNotBlank(request.description())) {
+            product.updateDescription(request.description());
+        }
+
+        if (request.canNegotiate() != null) {
+            product.updateCanNegotiate(request.canNegotiate());
+        }
+
+        if (request.saleEndDate() != null) {
+            validateProductSaleEndDate(request.saleEndDate(), order.getCheckInDate());
+            product.updateSaleEndDate(request.saleEndDate());
+        }
+
+        if (request.isAutoCancel() != null) {
+            product.updateIsAutoCancel(request.isAutoCancel());
+        }
+
+        return ProductIdResponse.from(product);
+    }
+
     private void validateProductSaveRequest(
         ProductSaveRequest request,
         Order order
@@ -84,7 +133,7 @@ public class ProductService {
     }
 
     private void validateProductPrice(int price, int purchasePrice) {
-        if (price > purchasePrice) {
+        if (price > purchasePrice || price <= 0) {
             throw new SellingPriceRangeException();
         }
     }
@@ -95,6 +144,12 @@ public class ProductService {
         if (saleEndDate.isBefore(LocalDate.now())
             || saleEndDate.isAfter(checkInDate)) {
             throw new SaleEndDateRangeException();
+        }
+    }
+
+    private void validateUpdatableProduct(Product product) {
+        if (product.getStatus() == CANCELED || product.getStatus() == SOLD_OUT) {
+            throw new InvalidStatusProductUpdateException();
         }
     }
 }
