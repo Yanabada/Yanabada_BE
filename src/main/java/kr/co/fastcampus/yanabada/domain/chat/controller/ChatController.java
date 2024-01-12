25647665f9b769aae4c8f06f13cc962efe93e3b5
JuHyeonPark <parkjh080@naver.com>
2024-01-12 17:47:33 +0900

    refactor: 채팅룸 생성 API 리팩토링
    채팅룸 생성 API 리팩토링

--- src/main/java/kr/co/fastcampus/yanabada/domain/chat/controller/ChatController.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/chat/controller/ChatController.java
@@ -2,10 +2,11 @@ package kr.co.fastcampus.yanabada.domain.chat.controller;
 
 import kr.co.fastcampus.yanabada.common.response.ResponseBody;
 import kr.co.fastcampus.yanabada.domain.chat.dto.request.ChatRoomSaveRequest;
-import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomIdResponse;
+import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomInfoResponse;
 import kr.co.fastcampus.yanabada.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class ChatController {
     private final ChatService chatService;
 
     @PostMapping
-    public ResponseBody<ChatRoomIdResponse> addChatRoom(ChatRoomSaveRequest request){
+    public ResponseBody<ChatRoomInfoResponse> addChatRoom(@RequestBody ChatRoomSaveRequest request){
         return ResponseBody.ok(chatService.saveChatRoom(request));
     }
 }
