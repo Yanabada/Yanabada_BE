03bc5f97ce1ff2d98dbca56157acec6a28c5b5df
JuHyeonPark <parkjh080@naver.com>
2024-01-13 13:15:47 +0900

    feat: 중복된 방을 생성하지 않기 위한 id로 방을 찾는 메소드 추가
    중복된 방을 생성하지 않기 위한 id로 방을 찾는 메소드 추가

--- src/main/java/kr/co/fastcampus/yanabada/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/chat/repository/ChatRoomRepository.java
@@ -1,7 +1,12 @@
 package kr.co.fastcampus.yanabada.domain.chat.repository;
 
+import java.util.Optional;
 import kr.co.fastcampus.yanabada.domain.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    Optional<ChatRoom> findByProductIdAndSellerIdAndBuyerId(
+        Long productId, Long sellerId, Long buyerId
+    );
 }
