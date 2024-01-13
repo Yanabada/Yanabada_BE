41934696aa499ee57c752d787dea5ba3bbf94c9c
JuHyeonPark <parkjh080@naver.com>
2024-01-14 00:18:05 +0900

    feat: 컨트롤러 - 채팅룸 마지막 확인 시간 수정 API 구현
    채팅룸 마지막 확인 시간 수정 API 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/chat/controller/ChatController.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/chat/controller/ChatController.java
@@ -2,15 +2,18 @@ package kr.co.fastcampus.yanabada.domain.chat.controller;
 
 import java.util.List;
 import kr.co.fastcampus.yanabada.common.response.ResponseBody;
+import kr.co.fastcampus.yanabada.domain.chat.dto.request.ChatRoomModifyRequest;
 import kr.co.fastcampus.yanabada.domain.chat.dto.request.ChatRoomSaveRequest;
 import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatMessageInfoResponse;
 import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomInfoResponse;
+import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomModifyResponse;
 import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomSummaryResponse;
 import kr.co.fastcampus.yanabada.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +46,13 @@ public class ChatController {
     ) {
         Long memberId = 1L;
         return ResponseBody.ok(chatService.getChatRoomMessages(memberId, chatRoomCode));
+    }
+
+    @PutMapping
+    public ResponseBody<ChatRoomModifyResponse> modifyChatRoom(
+        @RequestBody ChatRoomModifyRequest request
+    ){
+        Long memberId = 1L;
+        return ResponseBody.ok(chatService.updateChatRoom(memberId, request));
     }
 }
