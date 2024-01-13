2fbe1deb0a77afb61b722599c3cd6b4994859f07
JuHyeonPark <parkjh080@naver.com>
2024-01-13 13:15:08 +0900

    refactor: 서비스계층 - 채팅방 생성API 리팩토링
    서비스계층 - 채팅방 생성API 리팩토링

--- src/main/java/kr/co/fastcampus/yanabada/domain/chat/service/ChatService.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/chat/service/ChatService.java
@@ -2,6 +2,7 @@ package kr.co.fastcampus.yanabada.domain.chat.service;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
+import java.util.Optional;
 import kr.co.fastcampus.yanabada.common.exception.CannotNegotiateOwnProductException;
 import kr.co.fastcampus.yanabada.common.exception.NegotiationNotPossibleException;
 import kr.co.fastcampus.yanabada.domain.chat.dto.request.ChatRoomSaveRequest;
@@ -11,6 +12,7 @@ import kr.co.fastcampus.yanabada.domain.chat.repository.ChatMessageRepository;
 import kr.co.fastcampus.yanabada.domain.chat.repository.ChatRoomRepository;
 import kr.co.fastcampus.yanabada.domain.member.entity.Member;
 import kr.co.fastcampus.yanabada.domain.member.repository.MemberRepository;
+import kr.co.fastcampus.yanabada.domain.product.entity.enums.ProductStatus;
 import kr.co.fastcampus.yanabada.domain.product.entity.Product;
 import kr.co.fastcampus.yanabada.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -30,23 +32,20 @@ public class ChatService {
     private final ProductRepository productRepository;
 
     @Transactional
-    public ChatRoomInfoResponse saveChatRoom(ChatRoomSaveRequest request) {
+    public ChatRoomInfoResponse getOrSaveChatRoom(ChatRoomSaveRequest request) {
         Product product = productRepository.getProduct(request.productId());
         checkNegotiationPossibility(product);
         Long sellerId = product.getOrder().getMember().getId();
         Long buyerId = request.buyerId();
         checkIfOwnProduct(sellerId, buyerId);
-        Member seller = memberRepository.getMember(sellerId);
-        Member buyer = memberRepository.getMember(buyerId);
-        LocalDateTime now = LocalDateTime.now();
-        ChatRoom chatRoom = chatRoomRepository.save(
-            request.toEntity(product, seller, buyer, now, now)
-        );
-        return ChatRoomInfoResponse.from(chatRoom);
+        return getChatRoomInfoResponse(request, product, sellerId, buyerId);
     }
 
     private void checkNegotiationPossibility(Product product) {
-        if (!product.getCanNegotiate()) {
+        if (!product.getCanNegotiate()
+            || product.getStatus() == ProductStatus.CANCELED
+            || product.getStatus() == ProductStatus.BOOKING
+        ) {
             throw new NegotiationNotPossibleException();
         }
     }
@@ -54,6 +53,25 @@ public class ChatService {
     private void checkIfOwnProduct(Long sellerId, Long buyerId) {
         if (Objects.equals(sellerId, buyerId)) {
             throw new CannotNegotiateOwnProductException();
+        }
+    }
+
+    private ChatRoomInfoResponse getChatRoomInfoResponse(
+        ChatRoomSaveRequest request, Product product, Long sellerId, Long buyerId
+    ) {
+        Optional<ChatRoom> foundChatRoom = chatRoomRepository.findByProductIdAndSellerIdAndBuyerId(
+            product.getId(), sellerId, buyerId
+        );
+        if (foundChatRoom.isPresent()) {
+            return ChatRoomInfoResponse.from(foundChatRoom.get());
+        } else {
+            Member seller = memberRepository.getMember(sellerId);
+            Member buyer = memberRepository.getMember(buyerId);
+            LocalDateTime now = LocalDateTime.now();
+            ChatRoom chatRoom = chatRoomRepository.save(
+                request.toEntity(product, seller, buyer, now, now)
+            );
+            return ChatRoomInfoResponse.from(chatRoom);
         }
     }
 }
