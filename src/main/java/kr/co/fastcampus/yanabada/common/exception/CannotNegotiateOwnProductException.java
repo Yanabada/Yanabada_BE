d2b0b9600cebfea412cda0e55ec303d5add68e6a
JuHyeonPark <parkjh080@naver.com>
2024-01-13 19:39:50 +0900

    refactor: 예외 객체 리팩토링
    예외 객체 리팩토링

--- src/main/java/kr/co/fastcampus/yanabada/common/exception/CannotNegotiateOwnProductException.java
+++ src/main/java/kr/co/fastcampus/yanabada/common/exception/CannotNegotiateOwnProductException.java
@@ -1,9 +1,9 @@
 package kr.co.fastcampus.yanabada.common.exception;
 
-import kr.co.fastcampus.yanabada.common.response.ErrorCode;
+import static kr.co.fastcampus.yanabada.common.response.ErrorCode.CANNOT_NEGOTIATE_OWN_PRODUCT;
 
 public class CannotNegotiateOwnProductException extends BaseException {
     public CannotNegotiateOwnProductException() {
-        super(ErrorCode.CANNOT_NEGOTIATE_OWN_PRODUCT.getMessage());
+        super(CANNOT_NEGOTIATE_OWN_PRODUCT.getMessage());
     }
 }
