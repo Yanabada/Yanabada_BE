09be707f4f3de6bd8989aa1477f879da7bfc81a2
JuHyeonPark <parkjh080@naver.com>
2024-01-13 19:34:48 +0900

    feat: 레포지토리 - 채팅방 목록 조회 메소드 작성
    레포지토리 - 채팅방 목록 조회 메소드 작성

--- src/main/java/kr/co/fastcampus/yanabada/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/chat/repository/ChatRoomRepository.java
@@ -1,12 +1,22 @@
 package kr.co.fastcampus.yanabada.domain.chat.repository;
 
+import java.util.List;
 import java.util.Optional;
 import kr.co.fastcampus.yanabada.domain.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     Optional<ChatRoom> findByProductIdAndSellerIdAndBuyerId(
         Long productId, Long sellerId, Long buyerId
     );
+
+    @Query("SELECT cr FROM ChatRoom cr "
+        + "LEFT JOIN FETCH cr.messages "
+        + "WHERE (cr.buyer.id = :memberId AND cr.hasBuyerLeft = false) "
+        + "OR (cr.seller.id = :memberId AND cr.hasSellerLeft = false)"
+    )
+    List<ChatRoom> findChatRoomsByMemberId(@Param("memberId") Long memberId);
 }
