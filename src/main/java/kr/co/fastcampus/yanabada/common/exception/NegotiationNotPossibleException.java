ddb752a6d78c7692869c5cd9edd7a73832336c56
JuHyeonPark <parkjh080@naver.com>
2024-01-13 20:11:05 +0900

    refactor: NegotiationNotPossibleException 리팩토링
    컨벤션에 맞게 NegotiationNotPossibleException 리팩토링

--- src/main/java/kr/co/fastcampus/yanabada/common/exception/NegotiationNotPossibleException.java
+++ src/main/java/kr/co/fastcampus/yanabada/common/exception/NegotiationNotPossibleException.java
@@ -1,9 +1,9 @@
 package kr.co.fastcampus.yanabada.common.exception;
 
-import kr.co.fastcampus.yanabada.common.response.ErrorCode;
+import static kr.co.fastcampus.yanabada.common.response.ErrorCode.NEGOTIATION_NOT_POSSIBLE;
 
 public class NegotiationNotPossibleException extends BaseException {
     public NegotiationNotPossibleException() {
-        super(ErrorCode.NEGOTIATION_NOT_POSSIBLE.getMessage());
+        super(NEGOTIATION_NOT_POSSIBLE.getMessage());
     }
 }
