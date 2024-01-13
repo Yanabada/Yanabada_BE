6bb22db102a3d2cd18f0eeec4b864c49526a8edd
JuHyeonPark <parkjh080@naver.com>
2024-01-13 19:34:09 +0900

    feat: 컨트롤러 - 채팅방 목록 조회 API 구현
    컨트롤러 - 채팅방 목록 조회 API 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/chat/controller/ChatController.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/chat/controller/ChatController.java
@@ -3,12 +3,16 @@ package kr.co.fastcampus.yanabada.domain.chat.controller;
 import kr.co.fastcampus.yanabada.common.response.ResponseBody;
 import kr.co.fastcampus.yanabada.domain.chat.dto.request.ChatRoomSaveRequest;
 import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomInfoResponse;
+import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomSummaryResponse;
 import kr.co.fastcampus.yanabada.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/chats")
@@ -22,5 +26,13 @@ public class ChatController {
         @RequestBody ChatRoomSaveRequest request
     ) {
         return ResponseBody.ok(chatService.getOrSaveChatRoom(request));
+    }
+
+    @GetMapping
+    public ResponseBody<List<ChatRoomSummaryResponse>> getChatRooms(
+        // Principal 을 통해 로그인한 멤버 id를 가져올 예정
+    ) {
+        Long memberId = 1L;
+        return ResponseBody.ok(chatService.getChatRooms(memberId));
     }
 }
