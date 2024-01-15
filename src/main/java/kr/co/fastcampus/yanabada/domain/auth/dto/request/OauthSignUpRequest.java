588ca92905154b96c96887ba434f2ff0182e8568
tjdtn0219 <s_sjang@naver.com>
2024-01-16 00:21:15 +0900

    refactor: DTO EnumType Validatoin 수정

--- src/main/java/kr/co/fastcampus/yanabada/domain/auth/dto/request/OauthSignUpRequest.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/auth/dto/request/OauthSignUpRequest.java
@@ -3,19 +3,19 @@ package kr.co.fastcampus.yanabada.domain.auth.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import kr.co.fastcampus.yanabada.domain.member.entity.ProviderType;
+import lombok.NonNull;
 
 public record OauthSignUpRequest(
     @NotEmpty(message = "이메일 주소가 비어있을 수 없습니다")
     @Email
     String email,
-
     @NotEmpty(message = "사용자 이름이 비어있을 수 없습니다")
     String memberName,
     @NotEmpty(message = "닉네임이 비어있을 수 없습니다")
     String nickName,
     @NotEmpty(message = "휴대 전화 번호가 비어있을 수 없습니다")
     String phoneNumber,
-
+    @NonNull
     ProviderType provider,
     String deviceKey
 
