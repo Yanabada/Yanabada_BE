b04e65e45e9c7f9a01807a9b8d149fbc4af9a2b6
JuHyeonPark <parkjh080@naver.com>
2024-01-16 16:36:12 +0900

    feat: 상품 status에 따른 상품내역 조회 메소드 구현
    상품 status에 따른 상품내역 조회 메소드 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/product/repository/ProductRepository.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/product/repository/ProductRepository.java
@@ -2,7 +2,12 @@ package kr.co.fastcampus.yanabada.domain.product.repository;
 
 import kr.co.fastcampus.yanabada.common.exception.ProductNotFoundException;
 import kr.co.fastcampus.yanabada.domain.product.entity.Product;
+import kr.co.fastcampus.yanabada.domain.product.entity.enums.ProductStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository
     extends JpaRepository<Product, Long>, ProductRepositoryCustom {
@@ -10,4 +15,14 @@ public interface ProductRepository
     default Product getProduct(Long id) {
         return findById(id).orElseThrow(ProductNotFoundException::new);
     }
+
+    @Query("SELECT p FROM Product p WHERE "
+        + "(p.status = :status OR :status is null) ")
+    Page<Product> findByMemberId(
+        Long memberId,
+        @Param("status") ProductStatus status,
+        Pageable pageable
+    );
+
+
 }
