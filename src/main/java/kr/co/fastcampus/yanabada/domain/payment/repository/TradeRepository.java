f85b8a5690100b91e1a485cf6fbd1ca0e97f61e4
JuHyeonPark <parkjh080@naver.com>
2024-01-16 23:55:43 +0900

    feat: 상품과 거래 status로 거래 조회 메소드 구현
    상품과 거래 status로 거래 조회 메소드 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/payment/repository/TradeRepository.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/payment/repository/TradeRepository.java
@@ -1,8 +1,10 @@
 package kr.co.fastcampus.yanabada.domain.payment.repository;
 
 import java.util.List;
+import java.util.Optional;
 import kr.co.fastcampus.yanabada.common.exception.TradeNotFoundException;
 import kr.co.fastcampus.yanabada.domain.payment.entity.Trade;
+import kr.co.fastcampus.yanabada.domain.payment.entity.enums.TradeStatus;
 import kr.co.fastcampus.yanabada.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,4 +15,6 @@ public interface TradeRepository extends JpaRepository<Trade, Long> {
     default Trade getTrade(Long id) {
         return findById(id).orElseThrow(TradeNotFoundException::new);
     }
+
+    Optional<Trade> findByProductAndStatus(Product product, TradeStatus status);
 }
