ed6d2dfda9695d626f30c6d1a04be6054e893cc1
KyuCheol Hwang <iambigman97@naver.com>
2024-01-16 17:33:16 +0900

    refactor: 예약 찾는 메소드 repository 에 분리
    예약 찾는 메소드 repository 에 분리

--- src/main/java/kr/co/fastcampus/yanabada/domain/order/repository/OrderRepository.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/order/repository/OrderRepository.java
@@ -1,9 +1,13 @@
 package kr.co.fastcampus.yanabada.domain.order.repository;
 
+import kr.co.fastcampus.yanabada.common.exception.OrderNotFoundException;
 import kr.co.fastcampus.yanabada.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository
     extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 
+    default Order getOrder(Long id) {
+        return findById(id).orElseThrow(OrderNotFoundException::new);
+    }
 }
