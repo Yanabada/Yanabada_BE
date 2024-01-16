7852e7be9cc1253c942da62fd052cf48e5bc13f2
JuHyeonPark <parkjh080@naver.com>
2024-01-16 22:48:47 +0900

    feat: 판매내역 조회 응답 DTO구현
    판매내역 조회 응답 DTO구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/product/dto/response/ProductHistoryInfoResponse.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/product/dto/response/ProductHistoryInfoResponse.java
@@ -1,9 +1,11 @@
 package kr.co.fastcampus.yanabada.domain.product.dto.response;
 
-import kr.co.fastcampus.yanabada.domain.product.entity.enums.ProductStatus;
-
 import java.time.LocalDate;
+import kr.co.fastcampus.yanabada.domain.product.entity.Product;
+import kr.co.fastcampus.yanabada.domain.product.entity.enums.ProductStatus;
+import lombok.Builder;
 
+@Builder
 public record ProductHistoryInfoResponse(
     Long id,
     String image,
@@ -13,4 +15,16 @@ public record ProductHistoryInfoResponse(
     Integer sellingPrice,
     ProductStatus status
 ) {
+
+    public static ProductHistoryInfoResponse from(Product product) {
+        return ProductHistoryInfoResponse.builder()
+            .id(product.getId())
+            .image(product.getOrder().getRoom().getAccommodation().getImage())
+            .accommodationName(product.getOrder().getRoom().getAccommodation().getName())
+            .roomName(product.getOrder().getRoom().getName())
+            .saleEnd(product.getSaleEndDate())
+            .sellingPrice(product.getPrice())
+            .status(product.getStatus())
+            .build();
+    }
 }
