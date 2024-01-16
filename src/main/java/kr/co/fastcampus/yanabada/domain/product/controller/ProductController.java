a648ad140f5fab4a4e228e77e70dac9788142668
JuHyeonPark <parkjh080@naver.com>
2024-01-16 16:35:34 +0900

    feat: 판매내역 조회 API 구현
    판매내역 페이지 dto 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/product/controller/ProductController.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/product/controller/ProductController.java
@@ -5,12 +5,18 @@ import kr.co.fastcampus.yanabada.common.response.ResponseBody;
 import kr.co.fastcampus.yanabada.domain.product.dto.request.ProductPatchRequest;
 import kr.co.fastcampus.yanabada.domain.product.dto.request.ProductSaveRequest;
 import kr.co.fastcampus.yanabada.domain.product.dto.request.ProductSearchRequest;
+import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductHistoryInfoResponse;
+import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductHistoryPageResponse;
 import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductIdResponse;
 import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductInfoResponse;
 import kr.co.fastcampus.yanabada.domain.product.dto.response.ProductSummaryPageResponse;
+import kr.co.fastcampus.yanabada.domain.product.entity.enums.ProductStatus;
 import kr.co.fastcampus.yanabada.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -62,6 +69,18 @@ public class ProductController {
     ) {
         return ResponseBody.ok(
             productService.updateProduct(1L, productId, request)
+        );
+    }
+
+    @GetMapping("/own")
+    public ResponseBody<ProductHistoryPageResponse> getOwnProduct(
+        @RequestParam("status") ProductStatus status,
+        @PageableDefault(size = 20, sort = "registeredDate", direction = Sort.Direction.DESC)
+        Pageable pageable
+    ) {
+        Long memberId = 1L;
+        return ResponseBody.ok(
+            productService.getOwnProduct(memberId, status, pageable)
         );
     }
 }
