680ce71947fef735f8a0d90a95a13412635e7aa5
JuHyeonPark <parkjh080@naver.com>
2024-01-17 18:39:10 +0900

    refactor: 에러 코드 명 수정에 따른 예외명 수정
    에러 코드 명 수정에 따른 예외명 수정

--- src/main/java/kr/co/fastcampus/yanabada/common/exception/IllegalTradeStatusException.java
+++
@@ -1,9 +0,0 @@
-package kr.co.fastcampus.yanabada.common.exception;
-
-import static kr.co.fastcampus.yanabada.common.response.ErrorCode.ILLEGAL_TRADE_STATUS;
-
-public class IllegalTradeStatusException extends BaseException {
-    public IllegalTradeStatusException() {
-        super(ILLEGAL_TRADE_STATUS.getMessage());
-    }
-}
