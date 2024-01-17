c08291ebc57012bc3834d641f154127139fc3f36
JuHyeonPark <parkjh080@naver.com>
2024-01-17 23:43:59 +0900

    feat: 거래 멤버 역할과 STATUS 에 따른 거래 내역 조회 메소드 구현
    거래 멤버 역할과 STATUS 에 따른 거래 내역 조회 메소드 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/payment/repository/TradeRepository.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/payment/repository/TradeRepository.java
@@ -2,9 +2,16 @@ package kr.co.fastcampus.yanabada.domain.payment.repository;
 
 import java.util.List;
 import kr.co.fastcampus.yanabada.common.exception.TradeNotFoundException;
+import kr.co.fastcampus.yanabada.domain.member.entity.Member;
 import kr.co.fastcampus.yanabada.domain.payment.entity.Trade;
+import kr.co.fastcampus.yanabada.domain.payment.entity.enums.TradeRole;
+import kr.co.fastcampus.yanabada.domain.payment.entity.enums.TradeStatus;
 import kr.co.fastcampus.yanabada.domain.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
 
@@ -13,4 +20,17 @@ public interface TradeRepository extends JpaRepository<Trade, Long> {
     default Trade getTrade(Long id) {
         return findById(id).orElseThrow(TradeNotFoundException::new);
     }
+
+    @Query("SELECT t FROM Trade t "
+        + "WHERE (:role = 'SELLER' AND t.seller = :member OR "
+        + ":role = 'BUYER' AND t.buyer = :member) "
+        + "AND (:status IS NULL OR t.status = :status)")
+    Page<Trade> findByMemberRoleAndStatus(
+        @Param("member") Member member,
+        @Param("role") TradeRole role,
+        @Param("status")TradeStatus status,
+        Pageable pageable
+    );
+
+
 }
