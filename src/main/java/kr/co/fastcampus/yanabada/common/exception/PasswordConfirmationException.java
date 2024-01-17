be1cdeee851f4720b12533502c48567bdc79a793
deltawing71911 <deltawing71911@gmail.com>
2024-01-17 20:47:33 +0900

    refactor: 리뷰독 체크스타일 수정
    resolved #86

--- src/main/java/kr/co/fastcampus/yanabada/common/exception/PasswordConfirmationException.java
+++ src/main/java/kr/co/fastcampus/yanabada/common/exception/PasswordConfirmationException.java
@@ -3,5 +3,7 @@ package kr.co.fastcampus.yanabada.common.exception;
 import static kr.co.fastcampus.yanabada.common.response.ErrorCode.PASSWORD_CONFIRMATION_DOES_NOT_MATCH;
 
 public class PasswordConfirmationException extends BaseException {
-    public PasswordConfirmationException() { super(PASSWORD_CONFIRMATION_DOES_NOT_MATCH.getMessage()); }
+    public PasswordConfirmationException() {
+        super(PASSWORD_CONFIRMATION_DOES_NOT_MATCH.getMessage());
+    }
 }
