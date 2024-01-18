f837fb7ce0fd6028890cc9c5e52ce921a0d3d8e6
KyuCheol Hwang <iambigman97@naver.com>
2024-01-18 23:00:11 +0900

    refactor: 야놀자페이 이용내역 응답 DTO 수정
    야놀자페이 이용내역 응답 DTO 수정

--- src/main/java/kr/co/fastcampus/yanabada/domain/payment/dto/response/YanoljaPayHistorySummaryResponse.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/payment/dto/response/YanoljaPayHistorySummaryResponse.java
@@ -2,12 +2,14 @@ package kr.co.fastcampus.yanabada.domain.payment.dto.response;
 
 import java.time.LocalDateTime;
 import kr.co.fastcampus.yanabada.domain.payment.entity.YanoljaPayHistory;
+import kr.co.fastcampus.yanabada.domain.payment.entity.enums.ContentsType;
 import kr.co.fastcampus.yanabada.domain.payment.entity.enums.TransactionType;
 import lombok.Builder;
 
 @Builder
 public record YanoljaPayHistorySummaryResponse(
     Long historyId,
+    ContentsType contentsType,
     String contents,
     Long amount,
     TransactionType type,
@@ -16,6 +18,7 @@ public record YanoljaPayHistorySummaryResponse(
     public static YanoljaPayHistorySummaryResponse from(YanoljaPayHistory history) {
         return YanoljaPayHistorySummaryResponse.builder()
             .historyId(history.getId())
+            .contentsType(history.getContentsType())
             .contents(history.getContents())
             .amount(history.getTransactionAmount())
             .type(history.getTransactionType())
