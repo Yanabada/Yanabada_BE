ac1df98ef91f0fef051b2a36d9f7ef82c353fcb9
JuHyeonPark <parkjh080@naver.com>
2024-01-21 01:56:16 +0900

    feat: 알림 삭제 API 구현
    알림 삭제 API 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/notification/controller/NotificationController.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/notification/controller/NotificationController.java
@@ -2,6 +2,7 @@ package kr.co.fastcampus.yanabada.domain.notification.controller;
 
 import kr.co.fastcampus.yanabada.common.response.ResponseBody;
 import kr.co.fastcampus.yanabada.common.security.PrincipalDetails;
+import kr.co.fastcampus.yanabada.domain.notification.dto.response.NotificationIdResponse;
 import kr.co.fastcampus.yanabada.domain.notification.dto.response.NotificationPageResponse;
 import kr.co.fastcampus.yanabada.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +33,16 @@ public class NotificationController {
     ) {
         return ResponseBody.ok(
             notificationService.getNotifications(principalDetails.id(), pageable)
+        );
+    }
+
+    @DeleteMapping("/notificationId")
+    public ResponseBody<NotificationIdResponse> deleteNotification(
+        @AuthenticationPrincipal PrincipalDetails principalDetails,
+        @PathVariable("notificationId") Long notificationId
+    ) {
+        return ResponseBody.ok(
+            notificationService.deleteNotification(principalDetails.id(), notificationId)
         );
     }
 }
