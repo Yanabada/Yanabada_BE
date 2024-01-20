da37ecb4af7fe4f24c3ee4c8b33fb425e1c08b41
JuHyeonPark <parkjh080@naver.com>
2024-01-20 12:07:10 +0900

    refactor: 팩토리 패턴 메소드 추가
    팩토리 패턴 메소드 추가

--- src/main/java/kr/co/fastcampus/yanabada/domain/notification/dto/ChatNotificationDto.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/notification/dto/ChatNotificationDto.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
 import kr.co.fastcampus.yanabada.common.exception.JsonProcessFailedException;
+import kr.co.fastcampus.yanabada.domain.chat.entity.ChatRoom;
 import kr.co.fastcampus.yanabada.domain.member.entity.Member;
+import lombok.Builder;
 
+@Builder
 public record ChatNotificationDto(
     Member sender,
     Member receiver,
@@ -14,6 +17,21 @@ public record ChatNotificationDto(
     String chatRoomCode,
     String image
 ) {
+
+    public static ChatNotificationDto from(
+        Member sender,
+        Member receiver,
+        ChatRoom chatRoom
+    ) {
+        return ChatNotificationDto.builder()
+            .sender(sender)
+            .receiver(receiver)
+            .accommodationName(
+                chatRoom.getProduct().getOrder().getRoom().getAccommodation().getName()
+            )
+            .image(sender.getImageUrl())
+            .build();
+    }
 
     public String convertMapToJsonStr(ObjectMapper objectMapper) {
         Map<String, String> contentMap = new HashMap<>();
