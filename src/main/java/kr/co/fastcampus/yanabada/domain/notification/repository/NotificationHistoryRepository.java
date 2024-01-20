3376daae73ac967d34f825be057086ebbb6a7017
JuHyeonPark <parkjh080@naver.com>
2024-01-20 10:06:43 +0900

    feat: 멤버로 알림 찾는 메소드 구현
    멤버로 알림 찾는 메소드 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/notification/repository/NotificationHistoryRepository.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/notification/repository/NotificationHistoryRepository.java
@@ -1,8 +1,12 @@
 package kr.co.fastcampus.yanabada.domain.notification.repository;
 
+import kr.co.fastcampus.yanabada.domain.member.entity.Member;
 import kr.co.fastcampus.yanabada.domain.notification.entity.NotificationHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationHistoryRepository extends JpaRepository<NotificationHistory, Long> {
 
+    Page<NotificationHistory> findByMember(Member member, Pageable pageable);
 }
