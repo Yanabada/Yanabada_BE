272801372dc714c9d2ff3fb51a7a0db4c236f5f7
JuHyeonPark <parkjh080@naver.com>
2024-01-21 01:55:38 +0900

    feat: 알림 엔티티 조회 메소드 구현
    알림 엔티티 조회 메소드 구현

--- src/main/java/kr/co/fastcampus/yanabada/domain/notification/repository/NotificationHistoryRepository.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/notification/repository/NotificationHistoryRepository.java
@@ -1,5 +1,6 @@
 package kr.co.fastcampus.yanabada.domain.notification.repository;
 
+import kr.co.fastcampus.yanabada.common.exception.NotificationNotFoundException;
 import kr.co.fastcampus.yanabada.domain.member.entity.Member;
 import kr.co.fastcampus.yanabada.domain.notification.entity.NotificationHistory;
 import org.springframework.data.domain.Page;
@@ -9,4 +10,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface NotificationHistoryRepository extends JpaRepository<NotificationHistory, Long> {
 
     Page<NotificationHistory> findByReceiver(Member receiver, Pageable pageable);
+
+    default NotificationHistory getNotificationHistory(Long id) {
+        return findById(id).orElseThrow(NotificationNotFoundException::new);
+    }
 }
