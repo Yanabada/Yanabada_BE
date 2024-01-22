95375fece30ee3e6ccce2d7e556e28126703dd0f
JuHyeonPark <parkjh080@naver.com>
2024-01-22 16:39:36 +0900

    fix: 자바 유틸 랜덤 객체 빈 등록
    자바 유틸 랜덤 객체 빈 등록

--- src/main/java/kr/co/fastcampus/yanabada/common/config/AppConfig.java
+++ src/main/java/kr/co/fastcampus/yanabada/common/config/AppConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.util.Random;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +12,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public Random random() {
+        return new Random();
+    }
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
