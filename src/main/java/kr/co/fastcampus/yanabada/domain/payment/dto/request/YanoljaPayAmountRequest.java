df93dfbf711250674cfa823537950635f625a30e
KyuCheol Hwang <iambigman97@naver.com>
2024-01-22 16:11:57 +0900

    refactor: 요청 DTO 에 validation 메시지 추가
    요청 DTO 에 validation 메시지 추가

--- src/main/java/kr/co/fastcampus/yanabada/domain/payment/dto/request/YanoljaPayAmountRequest.java
+++ src/main/java/kr/co/fastcampus/yanabada/domain/payment/dto/request/YanoljaPayAmountRequest.java
@@ -4,16 +4,14 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 
 public record YanoljaPayAmountRequest(
-    @NotNull
-    @Positive
+    @NotNull(message = "금액은 필수로 입력하셔야 합니다.")
+    @Positive(message = "금액은 양수이어야 합니다.")
     Long amount,
 
-    @NotEmpty
-    @Size(min = 6, max = 6)
-    @Pattern(regexp = "^\\d{6}$")
+    @NotEmpty(message = "비밀번호는 필수로 입력하셔야 합니다.")
+    @Pattern(regexp = "^\\d{6}$", message = "비밀번호는 6자리 숫자이어야 합니다.")
     String simplePassword
 ) {
 
