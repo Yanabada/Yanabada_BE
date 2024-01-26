e87a7014583c3cf879202516023fb1ad04cea20a
tjdtn0219 <s_sjang@naver.com>
2024-01-26 13:03:28 +0900

    fix: 카카오 로그인 redirect URL 변경

--- src/main/java/kr/co/fastcampus/yanabada/common/security/oauth/Oauth2LoginSuccessHandler.java
+++ src/main/java/kr/co/fastcampus/yanabada/common/security/oauth/Oauth2LoginSuccessHandler.java
@@ -51,7 +51,7 @@ public class Oauth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
             authService.loginOauth(response, loginRequest, ProviderType.valueOf(provider));
         } else {
             /* 회원 가입 필요 */
-            String redirectUrl = "https://www.yanabada.com"
+            String redirectUrl = "http://localhost:5173"
                 + redirectPath
                 + "?email=" + email
                 + "&provider=" + provider;
