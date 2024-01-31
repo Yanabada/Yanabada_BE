643687fea319c5e16ad98c865b0a978070abbe16
JuHyeonPark <parkjh080@naver.com>
2024-01-31 16:43:59 +0900

    feat: 채팅 서비스 클래스 테스트 코드 작성
    채팅 서비스 클래스 테스트 코드 작성

--- src/test/java/kr/co/fastcampus/yanabada/domain/chat/service/ChatServiceTest.java
+++ src/test/java/kr/co/fastcampus/yanabada/domain/chat/service/ChatServiceTest.java
@@ -1,9 +1,33 @@
 package kr.co.fastcampus.yanabada.domain.chat.service;
 
+import static kr.co.fastcampus.yanabada.common.utils.TestUtils.createAccommodation;
+import static kr.co.fastcampus.yanabada.common.utils.TestUtils.createAccommodationOption;
+import static kr.co.fastcampus.yanabada.common.utils.TestUtils.createOrder;
+import static kr.co.fastcampus.yanabada.common.utils.TestUtils.createProduct;
+import static kr.co.fastcampus.yanabada.common.utils.TestUtils.createRoom;
+import static kr.co.fastcampus.yanabada.common.utils.TestUtils.createRoomOption;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import kr.co.fastcampus.yanabada.common.exception.CannotNegotiateOwnProductException;
+import kr.co.fastcampus.yanabada.domain.accommodation.entity.Accommodation;
+import kr.co.fastcampus.yanabada.domain.accommodation.entity.AccommodationOption;
+import kr.co.fastcampus.yanabada.domain.accommodation.entity.Room;
+import kr.co.fastcampus.yanabada.domain.accommodation.entity.RoomOption;
+import kr.co.fastcampus.yanabada.domain.chat.dto.request.ChatRoomSaveRequest;
+import kr.co.fastcampus.yanabada.domain.chat.dto.response.ChatRoomInfoResponse;
+import kr.co.fastcampus.yanabada.domain.chat.entity.ChatRoom;
 import kr.co.fastcampus.yanabada.domain.chat.repository.ChatMessageRepository;
 import kr.co.fastcampus.yanabada.domain.chat.repository.ChatRoomRepository;
+import kr.co.fastcampus.yanabada.domain.member.entity.Member;
 import kr.co.fastcampus.yanabada.domain.member.repository.MemberRepository;
 import kr.co.fastcampus.yanabada.domain.notification.service.NotificationService;
+import kr.co.fastcampus.yanabada.domain.order.entity.Order;
+import kr.co.fastcampus.yanabada.domain.product.entity.Product;
 import kr.co.fastcampus.yanabada.domain.product.repository.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,8 +58,60 @@ public class ChatServiceTest {
     private ChatService chatService;
 
     @Test
-    @DisplayName("숙박 종료 날짜가 시작")
-    void
+    @DisplayName("채팅방 생성")
+    void getOrSaveChatRoom_success() {
+        Member seller = mock(Member.class);
+        Member buyer = mock(Member.class);
+        Accommodation accommodation = createAccommodation();
+        AccommodationOption accommodationOption = createAccommodationOption(accommodation);
+        accommodation.registerAccommodationOption(accommodationOption);
+        Room room = createRoom(accommodation);
+        RoomOption roomOption = createRoomOption(room);
+        room.registerRoomOption(roomOption);
+        accommodation.addRoom(room);
+        Order order = createOrder(room, seller);
+        Product product = createProduct(order);
+        ChatRoom chatRoom = mock(ChatRoom.class);
 
+        given(productRepository.getProduct(1L)).willReturn(product);
+        given(seller.getId()).willReturn(2L);
+        given(memberRepository.getMember(2L)).willReturn(seller);
+        given(memberRepository.getMember(1L)).willReturn(buyer);
+        when(chatRoomRepository.save(any())).thenReturn(chatRoom);
 
+        //when
+        ChatRoomSaveRequest request = new ChatRoomSaveRequest(1L, 1L);
+        ChatRoomInfoResponse result = chatService.getOrSaveChatRoom(request);
+
+        //then
+        assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("채팅방 생성 실패")
+    void getOrSaveChatRoom_fail() {
+        Member seller = mock(Member.class);
+        Member buyer = mock(Member.class);
+        Accommodation accommodation = createAccommodation();
+        AccommodationOption accommodationOption = createAccommodationOption(accommodation);
+        accommodation.registerAccommodationOption(accommodationOption);
+        Room room = createRoom(accommodation);
+        RoomOption roomOption = createRoomOption(room);
+        room.registerRoomOption(roomOption);
+        accommodation.addRoom(room);
+        Order order = createOrder(room, seller);
+        Product product = createProduct(order);
+        ChatRoom chatRoom = mock(ChatRoom.class);
+
+        given(productRepository.getProduct(1L)).willReturn(product);
+        given(seller.getId()).willReturn(1L);
+        given(memberRepository.getMember(1L)).willReturn(seller);
+        given(memberRepository.getMember(1L)).willReturn(buyer);
+
+        //when then
+        ChatRoomSaveRequest request = new ChatRoomSaveRequest(1L, 1L);
+        assertThrows(CannotNegotiateOwnProductException.class, () -> {
+            chatService.getOrSaveChatRoom(request);
+        });
+    }
 }
