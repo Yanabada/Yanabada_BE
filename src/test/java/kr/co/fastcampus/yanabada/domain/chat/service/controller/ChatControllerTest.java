fde654baa5cc238c206b84ca0e42cb1ae3bef9cd
JuHyeonPark <parkjh080@naver.com>
2024-01-31 18:45:42 +0900

    test: 채팅방 수정, 채팅방 삭제 통합 테스트 코드 작성
    채팅방 수정, 채팅방 삭제 통합 테스트 코드 작성

--- src/test/java/kr/co/fastcampus/yanabada/domain/chat/service/controller/ChatControllerTest.java
+++ src/test/java/kr/co/fastcampus/yanabada/domain/chat/service/controller/ChatControllerTest.java
@@ -1,10 +1,13 @@
 package kr.co.fastcampus.yanabada.domain.chat.service.controller;
 
 import static kr.co.fastcampus.yanabada.common.response.ErrorCode.CANNOT_NEGOTIATE_OWN_PRODUCT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import io.restassured.path.json.JsonPath;
 import kr.co.fastcampus.yanabada.common.security.WithMockMember;
 import kr.co.fastcampus.yanabada.common.utils.ApiTest;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 
 @AutoConfigureMockMvc
@@ -71,5 +76,87 @@ public class ChatControllerTest extends ApiTest {
             .andExpect(jsonPath("$.status").value("FAIL"))
             .andExpect(jsonPath("$.errorMessage")
                 .value(CANNOT_NEGOTIATE_OWN_PRODUCT.getMessage()));
+    }
+
+    @DisplayName("[API][PUT] 채팅방 수정 성공")
+    @Test
+    @WithMockMember
+    void modifyChatRoom_success() throws Exception {
+        //given
+        String url = "/v1/chats";
+        String request = """
+            {
+                "productId": 1,
+                "buyerId": 2
+            }
+            """;
+
+        ResultActions resultActions = mockMvc.perform(post(url)
+                .content(request)
+                .contentType(MEDIA_TYPE)
+            );
+        MvcResult mvcResult = resultActions.andReturn();
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        JsonPath jsonPath = JsonPath.from(responseBody);
+        String chatRoomCode = jsonPath.getString("data.chatRoomCode");
+
+        String secondUrl = "/v1/chats";
+        String secondRequest = """
+            {
+                "chatRoomCode": "%s"
+            }
+            """.formatted(chatRoomCode);
+
+        //when
+        mockMvc.perform(put(secondUrl)
+            .content(secondRequest)
+            .contentType(MEDIA_TYPE)
+        )
+            //then
+            .andExpect(status().is2xxSuccessful())
+            .andExpect(jsonPath("$.status").value("SUCCESS"))
+            .andExpect(jsonPath("$.data.chatRoomCode").value(chatRoomCode))
+            .andExpect(jsonPath("$.data.updatedMemberId").value(1L));
+    }
+
+    @DisplayName("[API][DELETE] 채팅방 삭제 성공")
+    @Test
+    @WithMockMember
+    void modifyOrDeleteChatRoom_success() throws Exception {
+        //given
+        String url = "/v1/chats";
+        String request = """
+            {
+                "productId": 1,
+                "buyerId": 2
+            }
+            """;
+
+        ResultActions resultActions = mockMvc.perform(post(url)
+            .content(request)
+            .contentType(MEDIA_TYPE)
+        );
+        MvcResult mvcResult = resultActions.andReturn();
+        String responseBody = mvcResult.getResponse().getContentAsString();
+        JsonPath jsonPath = JsonPath.from(responseBody);
+        String chatRoomCode = jsonPath.getString("data.chatRoomCode");
+
+        String secondUrl = "/v1/chats";
+        String secondRequest = """
+            {
+                "chatRoomCode": "%s"
+            }
+            """.formatted(chatRoomCode);
+
+        //when
+        mockMvc.perform(delete(secondUrl)
+                .content(secondRequest)
+                .contentType(MEDIA_TYPE)
+            )
+            //then
+            .andExpect(status().is2xxSuccessful())
+            .andExpect(jsonPath("$.status").value("SUCCESS"))
+            .andExpect(jsonPath("$.data.chatRoomCode").value(chatRoomCode))
+            .andExpect(jsonPath("$.data.updatedMemberId").value(1L));
     }
 }
