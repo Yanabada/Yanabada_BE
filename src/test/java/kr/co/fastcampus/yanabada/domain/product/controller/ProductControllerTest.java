255064451ef5e7d2291d6a3c7e99275b7da44c5f
KyuCheol Hwang <iambigman97@naver.com>
2024-01-31 15:07:26 +0900

    test: 상품 컨트롤러 테스트 코드 작성
    ProductController 테스트 코드 작성

--- src/test/java/kr/co/fastcampus/yanabada/domain/product/controller/ProductControllerTest.java
+++ src/test/java/kr/co/fastcampus/yanabada/domain/product/controller/ProductControllerTest.java
@@ -1,10 +1,14 @@
 package kr.co.fastcampus.yanabada.domain.product.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDate;
 import kr.co.fastcampus.yanabada.common.security.WithMockMember;
+import kr.co.fastcampus.yanabada.common.utils.ApiTest;
+import kr.co.fastcampus.yanabada.domain.product.repository.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,29 +19,32 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ProductControllerTest {
+public class ProductControllerTest extends ApiTest {
 
     private static final MediaType MEDIA_TYPE = MediaType.APPLICATION_JSON;
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ProductRepository productRepository;
+
     @DisplayName("[API][POST] 상품 등록 - 정상적인 등록 요청일 경우, 정상적으로 상품이 등록됩니다.")
     @Test
     @WithMockMember
-    void add_success() throws Exception {
+    void addProduct_success() throws Exception {
         //given
         String url = "/v1/products";
         String request = """
             {
                 "orderId": 1,
-                "price": "400000",
+                "price": 10000,
                 "description": "급하게 처분합니다. 네고 환영합니다!!!",
                 "canNegotiate": true,
-                "saleEndDate": "2024-01-26",
+                "saleEndDate": "%s",
                 "isAutoCancel": true
             }
-            """;
+            """.formatted(LocalDate.now().toString());
 
         //when
         mockMvc.perform(post(url)
@@ -46,7 +53,22 @@ public class ProductControllerTest {
             )
 
             //then
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.status").value("FAIL"));
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value("SUCCESS"));
+    }
+
+    @DisplayName("[API][GET] 상품 조회 - 정상적인 조회 요청일 경우, 정상적으로 상품이 조회됩니다.")
+    @Test
+    @WithMockMember
+    void getProduct_success() throws Exception {
+        //given
+        String url = "/v1/products/1";
+
+        //when
+        mockMvc.perform(get(url))
+
+            //then
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value("SUCCESS"));
     }
 }
